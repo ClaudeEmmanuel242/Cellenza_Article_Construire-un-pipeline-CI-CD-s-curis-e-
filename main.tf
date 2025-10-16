6639fb7b2dfe38fded3419fdfc98cac275645f0b
ClaudeEmmanuel <Claude.mahoukou@cellenza.com>
2025-10-16 10:26:09 +0200

    ✅ Deploy pipeline

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "azurerm_policy_definition" {
-  source                = "git::https://entreprisegroup@dev.azure.com/entreprisegroup/TEMPLATE/_git/terraform.azurerm_policy_definition?ref=v0.0.1"
-  exemple                   = var.env
+  source  = "git::https://entreprisegroup@dev.azure.com/entreprisegroup/TEMPLATE/_git/terraform.azurerm_policy_definition?ref=v0.0.1"
+  exemple = var.env
 }
 
 
